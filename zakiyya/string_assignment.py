5673f10d5a9bf43b6f850669fe9508508a4f5c71
Zakiyya <zakiyyasaid24@gmail.com>
2023-09-05 14:31:03 -0700

    string-assignment

---
+++ zakiyya/string_assignment.py
@@ -0,0 +1,34 @@
+s# 1. Write a short description of what you understand a string
+# to be, in your own words.
+
+string can be defined as a data type enclosed in a quotation. The quotation must be used continously inorder to achieve the goal of string or it would be incorrect. To do the process, it requires a vaiable which stores the data called STRING.
+
+
+
+# 2. Give 10 examples of strings
+
+name = 'suwaiba'
+age = 20
+gender = 'female'
+occupation = 'teacher'
+maritalStatus = 'married'
+religion = 'islam'
+complexion = 'dark'
+nationality = 'nigerian'
+tribe = 'hausa'
+address = 'kawo'
+
+# What do you understand by a variable?
+
+A variable can be defined as a storage for data which help for easy access. There are different types of variable which are good variable, bad varriable,recommended variable.
+
+# 4. Give 5 examples of bad variables names
+
+$age = 20
+6name ='musa'
+&tribe = 'yoruba'
+@place = 'kwara'
+^gender = 'female'
+
+
+ 
